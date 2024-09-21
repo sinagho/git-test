aaf46b8ebdf3f128cb30e40d362525fa43933cd5
Colorless Tsukuru Tazaki <sinaghorbani388@gmail.com>
2024-09-22 00:19:55 +0330

    codding files are added

--- test/test.py
+++ test/test.py
@@ -1,3 +1,5 @@
 import pandas as pd
+import numpy as np
 
-print(f"this is pandas Version {pd.__version__}")
+print(f"this is pandas Version {pd.__version__} \n")
+print(f"this is numpy Version {np.__version__}")
