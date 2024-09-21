8c1b4579d0f16c9e599f1797c5bf9927e6c548bf
Colorless Tsukuru Tazaki <sinaghorbani388@gmail.com>
2024-09-22 01:28:21 +0330

    zoj and fard is updated

--- geet.py
+++ geet.py
@@ -3,7 +3,9 @@ user_input = int(input("Enter the number"))
 
 if user_input % 2 == 0:
     print("Zoj")
+elif user_input % 5 == 0:
+    print("ok")
 else:
-    print("fard")
+    print("False")
 
 print("end")
