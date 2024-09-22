13464c971e60a4b698aa3cfa03d18d576f7422c2
Colorless Tsukuru Tazaki <sinaghorbani388@gmail.com>
2024-09-23 00:56:36 +0330

    address the bugs

--- geet.py
+++ geet.py
@@ -4,7 +4,9 @@ user_input = int(input("Enter the number"))
 if user_input % 2 == 0:
     print("Zoj")
 elif user_input % 5 == 0:
-    print("ok")
+    print("Taqsim bar 5")
+    for i in range(1,4):
+        print(i)
 else:
     print("False")
 
