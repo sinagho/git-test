d01ba26234db57731aaf0185adef83e60e7bda92
Colorless Tsukuru Tazaki <sinaghorbani388@gmail.com>
2024-09-28 13:14:39 +0330

    review-p1

--- geet.py
+++ geet.py
@@ -17,17 +17,13 @@ print("the code is ended.")
 def addition(x, y):
     return x + y
 
-<<<<<<< HEAD
-def area(x, y = 5):
-    return x + (y ** 3)
-=======
 def area(x, y, z):
     if z == 4:
         return x * y
     else: return (x * y) + 3
->>>>>>> non-FastForward-Conf
 
 def subtraction(x, y):
     return x - y
 
 print("*" * 40)
+print('I love you')
